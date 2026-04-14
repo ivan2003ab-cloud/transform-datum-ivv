1f7c85c296bf8f3403a169089e322b8a08ce7542
ivan2003ab <ivan2003ab@gmail.com>
2026-04-14 19:50:16 +0700

    1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ export default function Page() {
   return (
     <div>
       {/* HEADER PAGE */}
-      <div className="bg-gray-400 text-white px-4 py-2 mb-4">
-        Home / Hitung Parameter / <span className="font-semibold">Input</span>
-      </div>
+      
 
       {/* CONTENT */}
       <div>
-        <h1 className="text-xl font-bold">Input</h1>
+        <h1 className="text-xl font-bold">Hello!!</h1>
       </div>
     </div>
   );
