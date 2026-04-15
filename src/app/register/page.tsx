806f4a1aa6a422c30eb8f310ab733f5af1debd21
ivan2003ab <ivan2003ab@gmail.com>
2026-04-15 09:09:51 +0700

    2.3

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+export const dynamic = "force-dynamic";
 
 export default function RegisterPage() {
   const router = useRouter();
